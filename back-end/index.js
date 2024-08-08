7e714d12ec85fa4a2c585b92eaf8e80c1acc2285
Luciano Silva <lcnsilvajf@gmail.com>
2024-08-08 13:02:08 -0300

    feat: express adicionado

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,24 @@
 const db = require('./config/dbConnection.js');
+const express = require('express');
 
+const app = express();
+const PORT = 3000;
 
 db.authenticate().then(() => {
     console.log("Conectado ao banco de dados.")
 }).catch( (error) => {
     console.log("Erro ao conectar.", error);
 })
+
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("Gincana TECH API");
+})
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em: http:localhost:${PORT}`);
+})
+
+
+
