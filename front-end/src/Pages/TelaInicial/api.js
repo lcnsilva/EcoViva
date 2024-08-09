90cca14cf7815a9ba147d07962d8f6adf839244a
Ueslei Lima <uesleilima2003@gmail.com>
2024-08-08 23:58:16 -0300

    feat: Implementação de funcionalidades na tela inicial.

---
+++ front-end/src/Pages/TelaInicial/api.js
@@ -0,0 +1,31 @@
+const API_URL = 'http://localhost:3001/publicacoes';
+
+export const fetchPost = async () => {
+  const response = await fetch(API_URL);
+  return response.json();
+
+};
+
+export const deleteEventos = async (id) => {
+  await fetch(`${API_URL}/${id}`, { method: 'DELETE' });
+};
+
+export const createPost = async (post) => {
+  await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  });
+};
+
+export const updateEventos = async (id, post) => {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  });
+};
--- front-end/src/Pages/TelaInicial/index.js
+++ front-end/src/Pages/TelaInicial/index.js
@@ -1,170 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, CardBody, CardTitle, CardSubtitle, CardText, CardLink, Row, Col } from 'reactstrap';
-
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Row, Col, FormGroup, Modal, ModalHeader, ModalBody, Form, Label, Input, ModalFooter, Button } from 'reactstrap';
 import "./StyleInicial.css";
+import { FcPlus } from "react-icons/fc";
+import { createPost } from './api';
+import { fetchPost } from './api';
 
 function TelaInicial() {
+    const [post, setPost] = useState([]);
+
+    const [nome, setNome] = useState("");
+    const [topico, setTopico] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [contato, setContato] = useState("");
+    const [addShowModal, setAddShowModal] = useState(false);
+
+    useEffect(() => {
+        getPost();
+        const interval = setInterval(fetchPost, 2000);
+        return () => clearInterval(interval);
+
+    }, [])
+
+    const getPost = async () => {
+        const getPost = await fetchPost();
+        setPost(getPost);
+    };
+
+    const handleCadastroPost = async (e) => {
+        e.preventDefault();
+        await createPost({ nome, topico, descricao, contato });
+        setAddShowModal(false);
+        setNome('');
+        setTopico('');
+        setDescricao('');
+        setContato('');
+    }
 
     return (
         <>
             <h1 style={{ textAlign: 'center' }}>Tela inicial</h1>
+            <Button color="success" onClick={() => setAddShowModal(true)} style={{ float: 'right' }}><FcPlus /></Button>
 
             <br></br>
-
-
+            <Row>
+                <Col md={4}>
+                    <Modal isOpen={addShowModal} toggle={() => setAddShowModal(!addShowModal)}>
+                        <ModalHeader toggle={() => setAddShowModal(!addShowModal)}>Cadastro de post</ModalHeader>
+                        <ModalBody>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="nome">Nome</Label>
+                                    <Input type="text" value={nome} onChange={e => setNome(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="topico">Topico</Label>
+                                    <Input type="text" value={topico} onChange={e => setTopico(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="descricao">Descrição</Label>
+                                    <Input type="text" value={descricao} onChange={e => setDescricao(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="contato">Contato</Label>
+                                    <Input type="email" value={contato} onChange={e => setContato(e.target.value)} />
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="success" onClick={handleCadastroPost}>Cadastrar</Button>
+                            <Button color="secondary" onClick={() => setAddShowModal(false)}>Cancelar</Button>
+                        </ModalFooter>
+                    </Modal>
+                </Col>
+            </Row>
             <div className='container__telaInicial'>
                 <div className="card">
-
                     <Row>
-                        <Col style={{marginTop: '30px'}}>
-                            <Card
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                </CardBody>
-                                <img
-                                    alt="Card cap"
-                                    src="https://picsum.photos/318/180"
-                                    width="100%"
-                                />
-                                <CardBody>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <CardLink href="#">
-                                        Card Link
-                                    </CardLink>
-                                    <CardLink href="#">
-                                        Another Link
-                                    </CardLink>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col  style={{marginTop: '30px'}}>
-                            <Card
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                </CardBody>
-                                <img
-                                    alt="Card cap"
-                                    src="https://picsum.photos/318/180"
-                                    width="100%"
-                                />
-                                <CardBody>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <CardLink href="#">
-                                        Card Link
-                                    </CardLink>
-                                    <CardLink href="#">
-                                        Another Link
-                                    </CardLink>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col  style={{marginTop: '30px'}}>
-                            <Card
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                </CardBody>
-                                <img
-                                    alt="Card cap"
-                                    src="https://picsum.photos/318/180"
-                                    width="100%"
-                                />
-                                <CardBody>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <CardLink href="#">
-                                        Card Link
-                                    </CardLink>
-                                    <CardLink href="#">
-                                        Another Link
-                                    </CardLink>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col style={{marginTop: '30px'}}>
-                            <Card
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                </CardBody>
-                                <img
-                                    alt="Card cap"
-                                    src="https://picsum.photos/318/180"
-                                    width="100%"
-                                />
-                                <CardBody>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <CardLink href="#">
-                                        Card Link
-                                    </CardLink>
-                                    <CardLink href="#">
-                                        Another Link
-                                    </CardLink>
-                                </CardBody>
-                            </Card>
-                        </Col>
+                        {post.map((post, index) => (
+                            <Col style={{ marginTop: '30px' }}>
+                                <Card
+                                    style={{
+                                        width: '18rem'
+                                    }}
+                                >
+                                    <CardBody>
+
+                                        <CardTitle tag="h5">
+                                            {post.nome}
+                                        </CardTitle>
+                                        <CardSubtitle
+                                            className="mb-2 text-muted"
+                                            tag="h6"
+                                        >
+                                            {post.topico}
+                                        </CardSubtitle>
+                                    </CardBody>
+                                    <img
+                                        alt="Card cap"
+                                        src="https://picsum.photos/318/180"
+                                        width="100%"
+                                    />
+                                    <CardBody>
+                                        <CardText>
+                                            {post.descricao}
+                                        </CardText>
+                                        <CardText>
+                                            {post.contato}
+                                        </CardText>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        ))}
+
                     </Row>
-                    <div className='group'>
-
-
-
-
-
-                    </div>
                 </div>
             </div>
         </>
