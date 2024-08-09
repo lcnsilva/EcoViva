0135d0c978b0c86f239f0d635ef05afa8d05599d
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2024-08-08 23:14:35 -0300

    Feat: Adicionado Fetch de eventos

--- front-end/src/Pages/PostEventos/index.js
+++ front-end/src/Pages/PostEventos/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createEventos } from './api';
+import { useNavigate } from 'react-router-dom';
 
-function PostEventos() {
+    const PostEventos = () => {
+        const [titulo, setTitulo] = useState('');
+        const [nome_organizador, setNome_organizador] = useState('');
+        const [endereco, setEndereco] = useState('');
+        const [descricao, setDescricao] = useState('');
+        const navigate = useNavigate();
+      
+        const handleSubmit = async (e) => {
+          e.preventDefault();
+          await createEventos({ titulo, nome_organizador, endereco, descricao });
+          setNome_organizador('');
+          setTitulo('');
+          setEndereco('');
+          setDescricao('');
+          navigate('/');
+        };
+    
+
     return (
         <>
         <h1>tela de postagem de eventos</h1>
 
         </>
     );
-  }
   
+};
   export default PostEventos;
