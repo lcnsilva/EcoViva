e9740fc141fc37f0a33a41c32e7849235d430fb9
Luciano Silva <lcnsilvajf@gmail.com>
2024-08-08 22:51:24 -0300

    fix: eventos routes fixed

--- back-end/routes/eventosRoutes.js
+++ back-end/routes/eventosRoutes.js
@@ -4,8 +4,8 @@ const EventoController = require('../controllers/eventoController.js');
 const routes = express.Router();
 
 routes.get('/eventos', EventoController.listarEventos);
-routes.post('/publicacoes', EventoController.cadastrarEvento);
-routes.put('/publicacoes/:id', EventoController.atualizarEvento);
-routes.delete('/publicacoes/:id', EventoController.excluirEvento);
+routes.post('/eventos', EventoController.cadastrarEvento);
+routes.put('/eventos/:id', EventoController.atualizarEvento);
+routes.delete('/eventos/:id', EventoController.excluirEvento);
 
 module.exports = routes;
