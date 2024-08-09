6f168646141f8a5d4d75efb4480b63048eaca4a9
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2024-08-09 11:33:03 -0300

    Feat: Página Eventos Feita

--- front-end/src/Pages/PostEventos/index.js
+++ front-end/src/Pages/PostEventos/index.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { createEventos, fetchEventos} from './api';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Row, Col, FormGroup, Modal, ModalHeader, ModalBody, Form, Label, Input, ModalFooter, Button } from 'reactstrap';
+import "../TelaInicial/StyleInicial.css";
+import { FcPlus } from "react-icons/fc";
+
 
     const PostEventos = () => {
         const [evento, setEvento] = useState([]);
@@ -8,6 +13,8 @@ import { createEventos, fetchEventos} from './api';
         const [nome_organizador, setNome_organizador] = useState('');
         const [endereco, setEndereco] = useState('');
         const [descricao, setDescricao] = useState('');
+        const [addShowModal, setAddShowModal] = useState(false);
+        const [nomePesquisa, setnomePesquisa] = useState("");
       
         const handleSubmitEventos = async (e) => {
           e.preventDefault();
@@ -29,13 +36,117 @@ import { createEventos, fetchEventos} from './api';
             const getEventoData = await fetchEventos();
             setEvento(getEventoData);
         };
+        
+        const handleSeachBynome = async (e) => {
+            e.preventDefault();
+            if (!nomePesquisa) {
+                await getEventos();
+                return;
+            }
+            try {
+                const response = await fetch(`http://localhost:3001/publicacoes/${nomePesquisa}`);
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar publicações!');
+                }
+                const data = await response.json();
+                console.log('Data received:', data);
+                setEvento(data);
+            } catch (error) {
+                console.error('Erro:', error);
+            }
     
+        }
 
         return (
             <>
-            <h1>tela de postagem de eventos</h1>
+            <br></br>
+            <Row>
+                <Col md={4}>
+                    <Modal isOpen={addShowModal} toggle={() => setAddShowModal(!addShowModal)}>
+                        <ModalHeader toggle={() => setAddShowModal(!addShowModal)}>Cadastro de evento</ModalHeader>
+                        <ModalBody>
+                            <Form>
+                                <FormGroup>
+                                    <Label for="titulo">Título</Label>
+                                    <Input type="text" value={titulo} onChange={e => setTitulo(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="nome">Nome</Label>
+                                    <Input type="text" value={nome_organizador} onChange={e => setNome_organizador(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="descricao">Descrição</Label>
+                                    <Input type="text" value={descricao} onChange={e => setDescricao(e.target.value)} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="endereco">Endereco</Label>
+                                    <Input type="email" value={endereco} onChange={e => setEndereco(e.target.value)} />
+                                </FormGroup>
+                            </Form>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="success" onClick={handleSubmitEventos}>Cadastrar</Button>
+                            <Button color="secondary" onClick={() => setAddShowModal(false)}>Cancelar</Button>
+                        </ModalFooter>
+                    </Modal>
+                </Col>
+            </Row>
 
-            </>
+            <div className='filter'>
+                <form onSubmit={handleSeachBynome} className='pesquisa'>
+                    <Input
+                        type='text'
+                        placeholder='Pesquisar'
+                        className='inputFiltro'
+                        value={nomePesquisa}
+                        onChange={e => setnomePesquisa(e.target.value)}
+                    />
+                </form>
+                <Button color="success" onClick={() => setAddShowModal(true)}><FcPlus /></Button>
+            </div>
+            <div className='container__telaInicial'>
+                <div className="card">
+                    <Row>
+                        {evento.map((evento, index) => (
+                            <Col style={{ marginTop: '30px' }}>
+                                <Card
+                                    style={{
+                                        width: '18rem'
+                                    }}
+                                >
+                                    <CardBody>
+
+                                        <CardTitle tag="h5">
+                                            {evento.titulo}
+                                        </CardTitle>
+                                        <CardSubtitle
+                                            className="mb-2 text-muted"
+                                            tag="h6"
+                                        >
+                                            {evento.nome}
+                                        </CardSubtitle>
+                                    </CardBody>
+                                    <img
+                                        alt="Card cap"
+                                        src="https://picsum.photos/318/180"
+                                        width="100%"
+                                    />
+                                    <CardBody>
+                                        <CardText>
+                                            {evento.descricao}
+                                        </CardText>
+                                        <CardText>
+                                            {evento.contato}
+                                        </CardText>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        ))}
+
+                    </Row>
+                </div>
+            </div>
+        </>
         );
   
 };
